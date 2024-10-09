876c0ce1db61b9c10827835c59cf7c143f62da39
zeryzez <lolmitoman@gmail.com>
2024-10-09 21:50:29 +0200

    ajout des components navbarre footer et header

---
+++ components/Header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<nav>
+    <div>
+        <h1>Blonbou Mathys</h1>
+    </div>
+
+    <ul>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Projet</a></li>
+        <li><a href="">A propos</a></li>
+    </ul>
+
+    <ul>
+        <a href="https://github.com/zeryzez">Github</a>
+        <a href="https://www.linkedin.com/in/mathys-blonbou-225a50257/">LinkedIn</a>
+    </ul>
+</nav>
